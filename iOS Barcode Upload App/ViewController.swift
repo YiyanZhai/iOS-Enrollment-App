01f712a88d3a7e99dc4a931452b9f3ff6f8371eb
YiyanZhai <zhaiyiyan17@gmail.com>
2023-05-12 16:56:11 -0400

    product view&buttons

--- iOS Barcode Upload App/ViewController.swift
+++ iOS Barcode Upload App/ViewController.swift
@@ -12,9 +12,62 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var barcode_button: UIButton!
     
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView3: UIImageView!
+    @IBOutlet weak var imageView4: UIImageView!
+    @IBOutlet weak var product_button_1: UIButton!
+    @IBOutlet weak var product_button_2: UIButton!
+    @IBOutlet weak var product_button_3: UIButton!
+    @IBOutlet weak var product_button_4: UIButton!
+    let imagePicker1 = UIImagePickerController()
+    let imagePicker2 = UIImagePickerController()
+    let imagePicker3 = UIImagePickerController()
+    let imagePicker4 = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imagePicker1.delegate = self
+        imagePicker2.delegate = self
+        imagePicker3.delegate = self
+        imagePicker4.delegate = self
+    }
+    
+    @IBAction func product_button_1(_ sender: UIButton) {
+        let actionSheet = UIAlertController(title: "Select Photo Source", message: nil, preferredStyle: .actionSheet)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { [weak self] _ in
+            guard let self = self else { return }
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                imagePicker1.sourceType = .camera
+                self.present(imagePicker1, animated: true, completion: nil)
+            } else {
+                let alert = UIAlertController(title: "Error", message: "Camera not available", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+        
+        let libraryAction = UIAlertAction(title: "Photo Library", style: .default) { [weak self] _ in
+            guard let self = self else { return }
+            imagePicker1.sourceType = .photoLibrary
+            self.present(imagePicker1, animated: true, completion: nil)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        actionSheet.addAction(cameraAction)
+        actionSheet.addAction(libraryAction)
+        actionSheet.addAction(cancelAction)
+        
+        present(actionSheet, animated: true, completion: nil)
+    }
+
+    @IBAction func product_button_2(_ sender: UIButton) {
+        imagePicker2.sourceType = .photoLibrary
+        present(imagePicker2, animated: true, completion: nil)
     }
     
     // This code creates an instance of UIAlertController with two actions, one for the camera and one for the photo library. It checks if the camera is available and presents the image picker with the camera source type if it is, or displays an error message if it's not available. If the user selects the photo library option, the image picker is presented with the photo library source type.
@@ -57,7 +110,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
 extension ViewController {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[.originalImage] as? UIImage {
-            imageView.image = image
+            switch picker {
+            case imagePicker1:
+                imageView1.image = image
+            case imagePicker2:
+                imageView2.image = image
+            default:
+                imageView.image = image
+            }
+
         }
         dismiss(animated: true, completion: nil)
     }
