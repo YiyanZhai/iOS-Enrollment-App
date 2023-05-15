e2fc4538312933c2f1ee0a88b315050f41fe7afc
YiyanZhai <zhaiyiyan17@gmail.com>
2023-05-15 00:07:44 -0400

    add next page and select image extra function

---
+++ iOS Barcode Upload App/SecondViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SecondViewController.swift
+//  iOS Barcode Upload App
+//
+//  Created by Yiyan Zhai on 5/14/23.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBOutlet weak var prev_button: UIButton!
+    
+    @IBAction func goToPrevPage(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let nextVC = storyboard.instantiateViewController(withIdentifier: "First")
+        self.navigationController?.pushViewController(nextVC, animated: false)
+    }
+}
