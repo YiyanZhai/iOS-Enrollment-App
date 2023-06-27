f0098b56687db1dd8b86a7acc97c8a85297f1cc3
YiyanZhai <zhaiyiyan17@gmail.com>
2023-06-26 21:33:24 -0400

    changes made

--- iOS Barcode Upload App/FinalViewController.swift
+++ iOS Barcode Upload App/FinalViewController.swift
@@ -3,12 +3,7 @@
 //  iOS Barcode Upload App
 //
 //  Created by Yiyan Zhai on 6/22/23.
-<<<<<<< HEAD
-// code for av foundation......
-=======
-// code for av foundation
->>>>>>> d1b263ba566599a00881e2c5497038c97638d792
-
+// av foundation codebase
 import UIKit
 //import PhotosUI
 import Foundation
